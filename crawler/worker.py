d87e0f1ec908919ba7cb167f42545c58af84a873
Stanley Lu <stanlehl@uci.edu>
2024-04-22 18:01:55 -0700

    Moved permission and crawl delay to worker.py

--- crawler/worker.py
+++ crawler/worker.py
@@ -1,6 +1,9 @@
 from threading import Thread
 
 from inspect import getsource
+from urllib.parse import urlparse, urljoin
+from urllib.robotparser import RobotFileParser
+
 from utils.download import download
 from utils import get_logger
 import scraper
@@ -8,6 +11,8 @@ import time
 
 
 class Worker(Thread):
+    permissions = {}
+    crawl_delays = {}
     def __init__(self, worker_id, config, frontier):
         self.logger = get_logger(f"Worker-{worker_id}", "Worker")
         self.config = config
@@ -16,19 +21,59 @@ class Worker(Thread):
         assert {getsource(scraper).find(req) for req in {"from requests import", "import requests"}} == {-1}, "Do not use requests in scraper.py"
         assert {getsource(scraper).find(req) for req in {"from urllib.request import", "import urllib.request"}} == {-1}, "Do not use urllib.request in scraper.py"
         super().__init__(daemon=True)
-        
+
     def run(self):
         while True:
             tbd_url = self.frontier.get_tbd_url()
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
                 break
-            resp = download(tbd_url, self.config, self.logger)
-            self.logger.info(
-                f"Downloaded {tbd_url}, status <{resp.status}>, "
-                f"using cache {self.config.cache_server}.")
-            scraped_urls = scraper.scraper(tbd_url, resp)
-            for scraped_url in scraped_urls:
-                self.frontier.add_url(scraped_url)
-            self.frontier.mark_url_complete(tbd_url)
-            time.sleep(self.config.time_delay)
+            domain = self.get_domain(tbd_url)
+            politeness_delay = self.get_politeness_delay(domain)
+            permission = self.get_permission(domain)
+            if permission:
+                if politeness_delay:
+                    time.sleep(politeness_delay)
+                resp = download(tbd_url, self.config, self.logger)
+                self.logger.info(
+                    f"Downloaded {tbd_url}, status <{resp.status}>, "
+                    f"using cache {self.config.cache_server}.")
+                scraped_urls = scraper.scraper(tbd_url, resp)
+                for scraped_url in scraped_urls:
+                    self.frontier.add_url(scraped_url)
+                self.frontier.mark_url_complete(tbd_url)
+                time.sleep(self.config.time_delay)
+
+    def get_domain(self, url):
+        return urlparse(url).netloc
+
+    def get_politeness_delay(self, domain):
+        if domain in self.crawl_delays:
+            return self.crawl_delays[domain]
+        else:
+            try:
+                robots_txt_url = urljoin(domain, "robots.txt")
+                robots_parser = RobotFileParser()
+                robots_parser.set_url(robots_txt_url)
+                robots_parser.read()
+                crawl_delay = robots_parser.crawl_delay('*')
+                self.crawl_delays[domain] = crawl_delay if crawl_delay else 0
+                return self.crawl_delays[domain]
+            except Exception as e:
+                self.logger.info(f"Error retrieving politeness delay: {e}")
+                return 0
+
+    def get_permission(self, domain):
+        if domain in self.permissions:
+            return self.permissions[domain]
+        else:
+            try:
+                robots_txt_url = urljoin(domain, "robots.txt")
+                robots_parser = RobotFileParser()
+                robots_parser.set_url(robots_txt_url)
+                robots_parser.read()
+                permission = robots_parser.can_fetch('*', domain)
+                self.permissions[domain] = permission
+                return permission
+            except Exception as e:
+                self.logger.info(f"Error retrieving permission: {e}")
